bb6c0c7a6775aec5efe257f9153ee4c702aec8d1
Jean Carlos Gallego <gallegomosquerajeancarlos@gmail.com>
2018-05-05 11:36:36 -0500

    Probando

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -7,15 +7,19 @@ public class HolaMundo {
         String sujeto = JOptionPane.showInputDialog("Put your name: ");
         saludar(sujeto);
     }
-    
+
     /**
      * Función que muestra un mensaje de dialogo con un saludo de acuerdo al parámetro
      * recibido
-     * 
-     * @param nombre 
+     *
+     * @param nombre
      */
     public static void saludar(String nombre){
         JOptionPane.showMessageDialog(null, "Hi "+nombre , "Greetings", JOptionPane.INFORMATION_MESSAGE);
     }
-    
+
+    public static void decirWara(String nombre){
+        JOptionPane.showMessageDialog(null, "Wara "+nombre , "Keloke", JOptionPane.INFORMATION_MESSAGE);
+    }
+
 }
