efed2f83bd498b7779e2540b63c26c9a790d31e9
Fanpay <andpay05@gmail.com>
2018-05-05 11:19:28 -0500

    Archivos vía upload

---
+++ HolaMundo.java
@@ -0,0 +1,21 @@
+
+import javax.swing.JOptionPane;
+
+
+public class HolaMundo {
+    public static void main(String args[]){
+        String sujeto = JOptionPane.showInputDialog("Put your name: ");
+        saludar(sujeto);
+    }
+    
+    /**
+     * Función que muestra un mensaje de dialogo con un saludo de acuerdo al parámetro
+     * recibido
+     * 
+     * @param nombre 
+     */
+    public static void saludar(String nombre){
+        JOptionPane.showMessageDialog(null, "Hi "+nombre , "Greetings", JOptionPane.INFORMATION_MESSAGE);
+    }
+    
+}
